6283efb89c8a23178aba23ae78d08e37614278eb
Israel Sampaio <israelsampaio28@gmail.com>
2021-09-28 18:12:18 -0400

    test: cria tests para componente Header

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+
   transform: {
-    "ˆ.+\\.(js|jsx|ts|tsx)": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)": "<rootDir>/node_modules/babel-jest",
   },
+
   setupFilesAfterEnv: ["<rootDir>/src/tests/setupTest.ts"],
+
   testEnvironment: "jsdom",
+
+  moduleNameMapper: {
+    "^.+\\.(css|sass|scss)$": "identity-obj-proxy",
+  },
 };
